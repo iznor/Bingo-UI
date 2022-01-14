6874125e5bcb2a4a020ea4ae11fd1c01e6803abd
iZnoR <76132625+iznor@users.noreply.github.com>
2022-01-14 11:27:55 +0200

    main-page buttons

--- src/components/Page/Main.jsx
+++ src/components/Page/Main.jsx
@@ -1,37 +1,25 @@
 import backButton from "../../assets/img/back.svg";
 import "./Main.scss";
 
-const style = {
-  display: "flex",
-  alignItems: "center",
-  fontWeight: "bold",
-  width: "100%",
-  minHeight: "812px",
-  justifyContent: "start",
-  border: "1px black solid",
-};
-const button = {
-  position: "absolute",
-  marginLeft: "35px",
-  padding: "0px",
-  backgroundImage: `url(${backButton})`,
-  backgroundColor: "transparent",
-  backgroundSize: "cover",
-  border: "none",
-  width: "35px",
-  height: "35px",
+const button = (divId, title) => {
+  return (
+    <button>
+      <h1>{title}</h1>
+      <div id={divId}></div>
+    </button>
+  );
 };
 
 function Main() {
   return (
-    <main style={style}>
+    <main>
       <div className="row">
-        <button id="reserve" className="mainButton"></button>
-        <button id="publish" className="mainButton"></button>
+        {button("reserve", "Find")}
+        {button("publish", "Add")}
       </div>
       <div className="row">
-        <button id="edit" className="mainButton"></button>
-        <button id="history" className="mainButton"></button>
+        {button("edit", "Edit")}
+        {button("history", "History")}
       </div>
     </main>
   );
