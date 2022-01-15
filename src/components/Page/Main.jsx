077b0ac6a0cda5c6445e6079e595546147f24118
iZnoR <76132625+iznor@users.noreply.github.com>
2022-01-15 06:55:33 +0200

    Now, each different page is a component

--- src/components/Page/Main.jsx
+++ src/components/Page/Main.jsx
@@ -1,43 +1,13 @@
 import "./Main.scss";
-
-const button = (divId, title) => {
-  return (
-    <button>
-      <h1>{title}</h1>
-      <div id={divId}></div>
-    </button>
-  );
-};
-
-const home = () => {
-  return (
-    <>
-      <div className="row">
-        {button("reserve", "Find")}
-        {button("publish", "Add")}
-      </div>
-      <div className="row">
-        {button("edit", "Edit")}
-        {button("history", "History")}
-      </div>
-    </>
-  );
-};
-
-const edit = () => {
-  return (
-    <>
-      <form></form>
-    </>
-  );
-};
+import Home from "./Home";
+import Edit from "./Edit";
 
 function Main() {
   return (
-    <main className="edit">
-      {/* {home()} */}
-      {edit()}
-    </main>
+    <>
+      <Home />
+      {/* <Edit /> */}
+    </>
   );
 }
 
