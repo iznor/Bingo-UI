076a8876e1d86dd4ba0cb0167840171dd661c3f9
Ayham10 <ayham.huss@gmail.com>
2022-01-16 17:23:51 +0200

    adding a Edit header in the headers

--- src/components/Page/Header.jsx
+++ src/components/Page/Header.jsx
@@ -2,14 +2,16 @@ import "./Header.scss";
 import Add from "./headers/Add";
 import HomeHeader from "./headers/Home";
 import IndexHeader from "./headers/Index";
+import Edit from "./headers/Edit";
 import Profile from "./headers/Profile";
 function Header() {
   return (
     <>
       {/* <IndexHeader /> */}
-      <Profile/>
+      {/* <Profile/> */}
       {/* <HomeHeader /> */}
       {/* <Add /> */}
+       <Edit />
     </>
   );
 }
---
+++ src/components/Page/headers/Edit.jsx
@@ -0,0 +1,12 @@
+
+
+function Edit() {
+    return (
+      <header>
+        <button></button>
+        <h1>Edit a parking </h1>
+      </header>
+    );
+  }
+  
+  export default Edit;
