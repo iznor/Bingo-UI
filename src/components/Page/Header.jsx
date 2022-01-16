2b1c45b5464169e090c188f40ce0427079263ec6
Ayham10 <ayham.huss@gmail.com>
2022-01-16 13:59:33 +0200

    no changes

--- src/components/Page/Header.jsx
+++ src/components/Page/Header.jsx
@@ -6,8 +6,8 @@ import Profile from "./headers/Profile";
 function Header() {
   return (
     <>
-      <IndexHeader />
-      {/* <Profile/> */}
+      {/* <IndexHeader /> */}
+      <Profile/>
       {/* <HomeHeader /> */}
       {/* <Add /> */}
     </>
