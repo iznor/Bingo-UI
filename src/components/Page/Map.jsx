47c4ebb21549191cb1452c4a8cf77f39fb4d4010
Ayham10 <ayham.huss@gmail.com>
2022-02-08 01:32:29 +0200

    fetching from database

--- src/components/Page/Map.jsx
+++ src/components/Page/Map.jsx
@@ -55,45 +55,10 @@ export default function App() {
   
   const [markers, setMarkers] = React.useState([]);
   const [selected, setSelected] = React.useState(null);
-  const [post, setPost] = React.useState(null);
 
-  // const ayham;
-  // const [notes, getNotes] = React.useState('');
   const url =  "http://localhost:8080/api/parkings";
-  //delete later -> GET this from DB
 
-    React.useEffect(() => {
-    axios.get(url).then((response) => {
-      setPost(response.data);
-    });
-  }, []);
-
-
-    if (post){
-      // console.log("hello");
-      post.forEach((e) => {
-        const currParkingId = Number(e.parkingId);
-        const currLat = Number(e.location.lat);
-        const currLng = Number(e.location.lng);
-        console.log(currParkingId);
-        console.log(currLat);
-       });
-      };
-
-
-      // console.log(currParkingId);
- 
-
-
-    
-    
-  const parkingData = [
-    { id: 1, lat: 32.0901, lng: 34.8036 },
-    { id: 2, lat: 32.123, lng: 34.891 },
-    { id: 3, lat: 32.1009, lng: 34.88036 },
-    { id: 4, lat: 32.2003, lng: 34.87836 },
-    { id: 5, lat: 32.3102, lng: 34.90036 },
-  ];
+  
 
   const onMapClick = React.useCallback((e) => {
     setSelected(null);
@@ -101,28 +66,42 @@ export default function App() {
 
   const mapRef = React.useRef();
   const onMapLoad = React.useCallback((map,post) => {
-
-    if(post){
-      console.log("hello");
-    }
     mapRef.current = map;
     //load all parking slots from database
-    parkingData.forEach((e) => {
-      const currParkingId = Number(e.id);
-      const currName = Number(e.name);
-      const currLat = Number(e.lat);
-      const currLng = Number(e.lng);
-      setMarkers((current) => [
-        ...current,
-        {
-          id: currParkingId,
-          lat: currLat,
-          lng: currLng,
-        },
-      ]);
+
+    
+    axios.get(url).then((response) => {
+      const dataMarkers = response.data;
+      console.log(dataMarkers);
+
+
+      dataMarkers.forEach((e) => {
+        const currParkingId = Number(e.parkingId);
+        const currLat = Number(e.location.lat);
+        const currLng = Number(e.location.lng);
+        const currStartDate = new Date(e.dateStart);
+        const currEndDate = new Date(e.dateEnd);
+        const currPrice = e.price;
+
+        setMarkers((current) => [
+          ...current,
+          {
+            id: currParkingId,
+            lat: currLat,
+            lng: currLng,
+            startDate: currStartDate,
+            endDate: currEndDate,
+            price: currPrice
+          },
+        ]);
+        
+       });
+
+
       return;
     });
   }, []);
+
 
   const panTo = React.useCallback(({ lat, lng }) => {
     mapRef.current.panTo({ lat, lng });
@@ -131,8 +110,6 @@ export default function App() {
 
   if (loadError) return "Error";
   if (!isLoaded) return "Loading...";
-  console.log("hello111");
-  console.log(post);
   return (
     <div className="map-container">
       <Search panTo={panTo} />
@@ -148,8 +125,11 @@ export default function App() {
       >
         {markers.map((marker) => (
           <Marker
-            key={`${marker.lat}-${marker.lng}`}
-            position={{ lat: marker.lat, lng: marker.lng }}
+            key={`${marker.id}`}
+            position={{ lat: marker.lat, lng: marker.lng}}
+            startDate={`${marker.startDate}`}
+            endDate={`${marker.endDate}`}
+            price = {`${marker.price}`}
             onClick={() => {
               setSelected(marker);
             }}
@@ -165,14 +145,17 @@ export default function App() {
         {selected ? (
               <InfoWindow
                 position={{ lat: selected.lat, lng: selected.lng }}
+                startDate={`${ selected.startDate}`}
+                endDate={`${ selected.endDate}`}
+                price = {`${selected.price}`}
                 onCloseClick={() => {
                   setSelected(null);
                 }}
                >
           <div className="info-box">
               <h2 style={{fontSize:"21px", paddingLeft:"25px" }}>Street name, City</h2>
-              <p style={{fontSize:"21px", paddingLeft:"25px" }}>Start Date - End Date</p>
-              <h2 style={{fontSize:"21px", paddingLeft:"25px" }}>Price $</h2>
+              <p style={{fontSize:"21px", paddingLeft:"25px" }}>{`${ selected.startDate.toLocaleDateString('en-GB')} - ${ selected.endDate.toLocaleDateString('en-GB')} `}</p>
+              <h2 style={{fontSize:"21px", paddingLeft:"25px" }}>{`${selected.price}`}$</h2>
               <div className="h1-cont">
               <button className="bingo-button">
                 bingo
