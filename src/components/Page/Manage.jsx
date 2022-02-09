8af4e5a4c4c6988b0263ce4c2471e255a309982a
iZnoR <76132625+iznor@users.noreply.github.com>
2022-02-09 19:30:12 +0200

    navigation fix, after filling the add new parking form

--- src/components/Page/Manage.jsx
+++ src/components/Page/Manage.jsx
@@ -3,10 +3,12 @@ import "./Edit.scss";
 import "./ParkCard.scss";
 import ParkCard from "./ParkCard.jsx";
 
+//const [myParkings, setMyParkings] = useState(BRING FROM DATABASE: select * from parkings where email==default@gmail.com)
+
 function Manage({ COMPONENT }) {
   return (
     <div className="my-parkings">
-      <ParkCard/>
+      <ParkCard />
     </div>
   );
 }
