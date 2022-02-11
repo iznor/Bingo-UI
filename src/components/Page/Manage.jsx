704b78e41fe701e89f46c0adf1833bdd880d45b6
Ayham10 <ayham.huss@gmail.com>
2022-02-11 22:14:16 +0200

    put request is working for a deafult example

--- src/components/Page/Manage.jsx
+++ src/components/Page/Manage.jsx
@@ -2,13 +2,34 @@ import "./Manage.scss";
 import "./Edit.scss";
 import "./ParkCard.scss";
 import ParkCard from "./ParkCard.jsx";
-
+import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import { set } from "date-fns/esm";
 //const [myParkings, setMyParkings] = useState(BRING FROM DATABASE: select * from parkings where email==default@gmail.com)
 
 function Manage({ COMPONENT }) {
+  const [userList, setUserList] = useState([]);
+ 
+  useEffect(() => {
+    const datab =  axios({
+      method: "Get",
+      url: "https://bingo-parking.herokuapp.com/api/parkings",
+    })
+    .then((datab) => {
+      const data = JSON.stringify(datab.data);
+      const obj = JSON.parse(data);
+      setUserList(obj);
+    })
+  }, []);
+  
+  const filteredPlayers = userList.filter((user) => user.email === "default@gmail.com");
+  console.log(filteredPlayers);
+
   return (
     <div className="my-parkings">
-      <ParkCard />
+      {/* {filteredPlayers.map(parking =>{ */}
+        return <ParkCard />
+        {/* })} */}
     </div>
   );
 }
