c36d02d0976b8c92098690982e757a77e9e8a181
Ayham10 <ayham.huss@gmail.com>
2022-02-12 14:49:45 +0200

    deleting patking is almost done

--- src/components/Page/Manage.jsx
+++ src/components/Page/Manage.jsx
@@ -3,12 +3,13 @@ import "./Edit.scss";
 import "./ParkCard.scss";
 import ParkCard from "./ParkCard.jsx";
 import axios from "axios";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState ,useCallback} from 'react';
 import { set } from "date-fns/esm";
 //const [myParkings, setMyParkings] = useState(BRING FROM DATABASE: select * from parkings where email==default@gmail.com)
 
 function Manage({ COMPONENT }) {
   const [parkingList, setParkingList] = useState([]);
+  // const [filteredParking, setFilteredParking] = useState([]);
  
   useEffect(() => {
     const datab =  axios({
@@ -18,17 +19,40 @@ function Manage({ COMPONENT }) {
     .then((datab) => {
       const data = JSON.stringify(datab.data);
       const obj = JSON.parse(data);
-      setParkingList(obj);
+      setParkingList(obj.filter((user) => user.email === "default@gmail.com"));
     })
   }, []);
   
-  const filteredParking = parkingList.filter((user) => user.email === "default@gmail.com");
-  console.log(filteredParking);
+  // if(parkingList.length >1){
+  //   console.log("hello");
+  //   setParkingList(parkingList.filter((user) => user.email === "default@gmail.com"));
+  //   console.log(parkingList);
+  // }
+  // setFilteredParking(parkingList.filter((user) => user.email === "default@gmail.com"));
+  // const filteredParking = parkingList;
+  // console.log(filteredParking);
+
+  const renderParking = useCallback(() => {
+    return parkingList.map((parking) => {
+        return <ParkCard key={parking.parkingId} {...parking} onDeleteClick={deleteParking} />;
+    });
+  }, [parkingList]);
+
+  const deleteParking = (parkingId) => {
+    setParkingList((prevState) => {
+      return prevState.filter((parking) => parking.parkingId !== parkingId);
+    });
+  };
+
   return (
       <div className="my-parkings">   
-      {filteredParking.map((result, i) => (
-        <ParkCard name={result} key={result.parkingId}></ParkCard>
-      ))}
+      {renderParking()}
+        {/* {parkingList.map((result, i) => (
+        <ParkCard name={result} key={result.parkingId} 
+        setParkingList={setParkingList}
+        parkingList = {parkingList}
+        ></ParkCard>
+       ))} */}
     </div>
   );
 }
--- src/components/Page/ParkCard.jsx
+++ src/components/Page/ParkCard.jsx
@@ -1,20 +1,55 @@
 //PARK CARD -> NOT HOME !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-import {Link} from "react-router-dom";
-const card = (address, dates) => {
+import React, { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
+// const card = (address, dates) => {
+//   return (
+//     <div className="card">
+//       <h1>{address}</h1>
+//       <h2>{dates}</h2>
+//       <h3>delete</h3>
+//     </div>
+//   );
+// };
+
+
+function ParkCard({ parkingId, person,lastName,phoneNumber,dateStart,dateEnd, location, price,email, onDeleteClick }) {
+  // console.log(parkingId);
+  // console.log(person.firstName);
+  // console.log(dateEnd);
+  // console.log(email);
+  // console.log(props.parkingList);
+  // const [newParkings, setnewParkings] = useState([]);
+  // setnewParkings(props.parkingList);
+
+  // const handleRemoveItem = e => {
+  //   console.log("delete");
+  //   const name = e.target.getAttribute("name");
+  //   console.log(name);
+  //   // updateList(list.filter(item => item.name !== name));
+  //   // console.log(props.name.person.firstName);
+  //   // props.setParkingList(props.parkingList.slice(props.parkingList.indexOf(e.target.name, 1)))
+  // }
   return (
-    <div className="card">
-      <h1>{address}</h1>
-      <h2>{dates}</h2>
-    </div>
-  );
-};
-function ParkCard(props ,address ,dates) {
-  return (
-      <div className="card-row" >
-        <Link to={"/manage/edit"}>
+    <div className="card-row" >
+      
+      {/* {props.parkingList.map(item => {
+        return (
+          <> */}
+          <div className="card">
+           <h1>{person.firstName}</h1>
+           <h2>{dateStart}</h2>
+            <span onClick={() => onDeleteClick(parkingId)} >
+              delete 
+           </span>
+            </div>
+          {/* </>
+        );
+      })} */}
+     
+      {/* <Link to={"/manage/edit"}>
           {card(props.name.person.firstName,props.name.dateStart)}
-        </Link>
-      </div>
+        </Link> */}
+    </div>
   );
 }
 export default ParkCard;
